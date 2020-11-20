ffb08a9229b3800c150bcd0b76f36a00e9b4b744
Dmitry Kuleshov <d.kuleshov@napopravku.ru>
2020-11-20 17:41:51 +0300

    Initial Commit

---
+++ FPSUtility/FPSUtility.h
@@ -0,0 +1,18 @@
+//
+//  FPSUtility.h
+//  FPSUtility
+//
+//  Created by Dmitry Kuleshov on 20.11.2020.
+//
+
+#import <Foundation/Foundation.h>
+
+//! Project version number for FPSUtility.
+FOUNDATION_EXPORT double FPSUtilityVersionNumber;
+
+//! Project version string for FPSUtility.
+FOUNDATION_EXPORT const unsigned char FPSUtilityVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <FPSUtility/PublicHeader.h>
+
+
